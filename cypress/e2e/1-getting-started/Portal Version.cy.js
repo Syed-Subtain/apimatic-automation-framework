63e3ef5dfdde8d708f7554557a96eb336d3249fb
Syed-Subtain <subtainshah2113@gmail.com>
2022-08-23 15:51:46 +0500

    Added Programing language absed portal testing

--- cypress/e2e/1-getting-started/Portal Version.cy.js
+++ cypress/e2e/1-getting-started/Portal Version.cy.js
@@ -7,34 +7,31 @@ describe("Apimatic Portal Testing",()=>{
    
     it("Should be able to iterate in different Versions of Portal Docs",()=>{
        
-        cy.visit('https://developer.greenbyte.com/v/2_1-beta')
-        cy.get('#version-select').find('option').each(($opn)=>{
-            const optionText = $opn.text()
-            const urlText = optionText.replace(/\./g,'_')
-            cy.get('#version-select').select(optionText) 
-            cy.url().should("include",urlText)
+        cy.visit('https://developer.greenbyte.com/v/2_1#/http/how-to-get-started')
+       // Validation for each Version of Portal
+        cy.get('#version-select')
+            .find('option')
+            .each(($opn)=>{
+                const optionText = $opn.text()
+                const urlText = optionText.replace(/\./g,'_')
+                cy.get('#version-select').select(optionText) 
+                cy.url().should("include",urlText)
+                //Validation of portal publishling for each available Language aginst in version
+                cy.xpath('//*[@id="apimatic-widget"]/div/div/div[2]/div[1]/div/div[2]/ul').click()
+                cy.get('.rc-menu.rc-menu-sub.rc-menu-vertical').find('li').its('length').then(len=>{
+                cy.log(len)
+                for(let k=1;k<=len;k++)
+               {   cy.xpath('//*[@id="apimatic-widget"]/div/div/div[2]/div[1]/div/div[2]/ul').click({force:true})
+                    cy.xpath('//*[@id="item_0$Menu"]/li['+ k +']').click({force:true})
+                    cy.wait(2000)
+                }   
+
+
+            })
+                
+                  
+
         })
-        
-        
-        
-        
-        /*cy.visit('https://developer.greenbyte.com/v/2_2-beta#/http')
-        cy.get('#version-select').find('option').its('length').then((len)=>{
-            cy.log(len)
-           for(let i=0; i<=len;i++)
-            {
-                let txt,result
-                cy.get('#version-select').select(i).then(($sel)=>{
-                    txt=$sel.text()
-                    result= txt.replace('.','_')
-                    cy.log($sel)
-                    cy.url().should("include",txt)
-                })
-  
-             }
-        }) */
-
-
-
     })
+    
 })
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -12,7 +12,7 @@
 // You can read more here:
 // https://on.cypress.io/configuration
 // ***********************************************************
-
+require('cypress-xpath')
 // Import commands.js using ES2015 syntax:
 import './commands'
 
