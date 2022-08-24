1d7a27d30e40862f109f959cc834e2e5a5ee0938
Syed-Subtain <subtainshah2113@gmail.com>
2022-08-24 10:56:13 +0500

    Urls file added in support folder

--- cypress/e2e/1-getting-started/Portal Version.cy.js
+++ cypress/e2e/1-getting-started/Portal Version.cy.js
@@ -1,13 +1,12 @@
-const urls = ['https://developer.commandalkon.io/v/2_0_0', 'https://www.apimatic.io/apidocs/ocxfulfillmentservices/v/1_2_0','https://www.chargelogic.com/connect/developer/']
-
-describe("Apimatic Portal Testing",()=>{
-    
-    Cypress.on("uncaught:exception", (err, runnable) => {
-        // returning false here prevents Cypress from
-        // failing the test
-        return false;
-      });
-      urls.forEach((url)=>{
+//const urls = ['https://developer.commandalkon.io/v/2_0_0', 'https://www.apimatic.io/apidocs/ocxfulfillmentservices/v/1_2_0','https://www.chargelogic.com/connect/developer/']
+import { testUrls } from '../../support/portalurl.js';
+Cypress.on("uncaught:exception", (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false;
+  });
+describe("Apimatic Portal Testing",()=>{    
+      testUrls.forEach((url)=>{
     it("Should be able to iterate in different Versions of Portal Docs",()=>{
        
         cy.visit(url)
