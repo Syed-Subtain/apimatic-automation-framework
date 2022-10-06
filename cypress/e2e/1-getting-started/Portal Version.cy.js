11d16d264c6fa167a962d9d092d16e1c03ebc6f5
Syed-Subtain <subtainshah2113@gmail.com>
2022-10-06 17:10:53 +0500

    Import APi through URL Update

--- cypress/e2e/1-getting-started/Portal Version.cy.js
+++ cypress/e2e/1-getting-started/Portal Version.cy.js
@@ -28,4 +28,35 @@ describe("Apimatic Portal Testing",()=>{
                 
       })          
 
+      it.only("Import an API using URL", () => {
+        cy.visit('https://www.apimatic.io/account/login')
+        cy.get('#Email').type('syed.subtain@apimatic.io')
+        cy.get('#js-onboarding-password-field').type('Welcome@1')
+        cy.get('.btn-primary').click()
+        cy.wait(7000)
+        cy.get('[ng-click="apiDashboardEventHandlers.importAPICliked()"]').contains('Import API').click({force:true});
+
+        // }).should("have.text", "Import API")
+        // cy.get('[ng-click="apiDashboardEventHandlers.importAPICliked()"] > span', {
+        //   timeout: 30000,
+        // }).click({ force: true });
+        // cy.get('[ng-show="modalUiState.activeView == \'screen_1\'"]').find("[ng-model=importUrl]").type("https://petstore.swagger.io/v2/swagger.json")
+       
+        cy.get('#apiImportModal').find('input[name="importUrl"]').type("https://petstore.swagger.io/v2/swagger.json")
+          cy.get("[class='btn btn-default']").contains('Import').click()
+          cy.get('#apiImportModal').find('[class="btn btn-default fileinput-exists"]',{timeout:5000}).click()
+          cy.contains("Validation Summary");
+        //find('input[name="importUrl"]').type("https://petstore.swagger.io/v2/swagger.json");
+       
+        // cy.wait(2000)
+        // cy.get("[class='btn btn-default']").contains('Import').click()
+        // cy.xpath('//*[@id="apiImportModal"]/div/div[5]/div/div[4]/input').click({
+        //   force: true,
+        // });
+        // cy.xpath('//*[@id="apiImportModal"]/div/div[5]/div/div[2]/div/div[1]/div[1]').contains('0')
+        // cy.xpath('//*[@id="apiImportModal"]/div/div[6]/div/div[4]/button').click({
+        //   force: true,
+        // });
+      });
+
 })
