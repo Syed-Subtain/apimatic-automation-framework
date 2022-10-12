e5a6830de4377a09fc54699fd76db6f3fa6e52d4
Syed-Subtain <subtainshah2113@gmail.com>
2022-10-12 11:05:46 +0500

    COde refactored

--- cypress/e2e/1-getting-started/Portal Version.cy.js
+++ cypress/e2e/1-getting-started/Portal Version.cy.js
@@ -28,36 +28,30 @@ describe("Apimatic Portal Testing",()=>{
                 
       })          
 
-      it.only("Import an API using URL", () => {
+      it("Import an API using URL", () => {
         cy.visit('https://www.apimatic.io/account/login')
         cy.get('#Email').type('syed.subtain@apimatic.io')
         cy.get('#js-onboarding-password-field').type('Welcome@1')
         cy.get('.btn-primary').click()
         cy.wait(7000)
+        //click on import API button
         cy.get('[ng-click="apiDashboardEventHandlers.importAPICliked()"]').contains('Import API').click();
-
-        // }).should("have.text", "Import API")
-        // cy.get('[ng-click="apiDashboardEventHandlers.importAPICliked()"] > span', {
-        //   timeout: 30000,
-        // }).click({ force: true });
-        // cy.get('[ng-show="modalUiState.activeView == \'screen_1\'"]').find("[ng-model=importUrl]").type("https://petstore.swagger.io/v2/swagger.json")
-        // cy.get('#apiImportModal').contains('Import API')
+        //Adding URL of spec
         cy.get('#apiImportModal').find('input[name="importUrl"]').type("https://petstore.swagger.io/v2/swagger.json",{force:true})
-          cy.get("[class='btn btn-default']").contains('Import').click({force:true})
+        //clicking on Import button  
+        cy.get("[class='btn btn-default']").contains('Import').click({force:true})
+        //click on proceed button
           cy.get('#apiImportModal').find('[ng-show="targetModal.importLoggingSumamry.success && totalValidationSummaryCount > 0"]',{timeout:5000}).click()
           cy.contains("Validation Summary");
+          //click on close button      
           cy.get('button').contains('Close').click({force:true})
-        //find('input[name="importUrl"]').type("https://petstore.swagger.io/v2/swagger.json");
-       
-        // cy.wait(2000)
-        // cy.get("[class='btn btn-default']").contains('Import').click()
-        // cy.xpath('//*[@id="apiImportModal"]/div/div[5]/div/div[4]/input').click({
-        //   force: true,
-        // });
-        // cy.xpath('//*[@id="apiImportModal"]/div/div[5]/div/div[2]/div/div[1]/div[1]').contains('0')
-        // cy.xpath('//*[@id="apiImportModal"]/div/div[6]/div/div[4]/button').click({
-        //   force: true,
-        // });
+        
       });
+
+      it.only("Import an API by uploading a spec file", () => {
+        cy.visit('https://www.apimatic.io/account/login')
+           
+      });
+
 
 })
